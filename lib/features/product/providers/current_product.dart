8d76057bbb06af7e09a79ff27aa1762cd6d54fbc
HasegawaShuhei <sh1027.dev@gmail.com>
2023-02-12 00:09:59 +0900

    不要なファイル削除

--- lib/features/product/providers/current_product.dart
+++
@@ -1,6 +0,0 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import '../models/product.dart';
-
-final currentProductProvider =
-    Provider.autoDispose<Product>((ref) => throw UnimplementedError());
